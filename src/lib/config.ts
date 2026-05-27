01fac4742231db515105e47f95452c4569414cd8
hafizbahtiar <hafizbahtiar98@gmail.com>
2026-05-28 00:47:30 +0800

    feat: update API_BASE_URL to use environment variable for flexibility

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "https://hono-workers.hafizbahtiar98.workers.dev/api/v1";
+export const API_BASE_URL =
+    import.meta.env.PUBLIC_API_URL || "https://hono-workers.hafizbahtiar98.workers.dev/api/v1";
