6842875a9b20135264d4d4c633a5acd052ea89cf
hafizbahtiar <hafizbahtiar98@gmail.com>
2026-05-29 01:54:56 +0800

    fix

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,6 @@
+const PRODUCTION_BROWSER_API_URL = "https://api.hafizbahtiar.com/api/v1";
+const PRERENDER_API_URL = "https://hono-workers.hafizbahtiar98.workers.dev/api/v1";
+
 export const API_BASE_URL =
-    import.meta.env.PUBLIC_API_URL || "https://api.hafizbahtiar.com/api/v1";
+    import.meta.env.PUBLIC_API_URL ||
+    (typeof window === "undefined" ? PRERENDER_API_URL : PRODUCTION_BROWSER_API_URL);
